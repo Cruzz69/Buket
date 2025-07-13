c5b63230b51720e6bd771113fc9e1e06703ff528
Cruzz69 <devnfordailyuse@gmail.com>
2025-07-14 02:02:43 +0530

    updating

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,29 @@
 function isAuthenticated(req, res, next) {
-  if (req.session && req.session.userId) next();
-  else res.redirect('../views/login.html');
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.redirect('/login'); 
+  }
 }
+
 function isAdmin(req, res, next) {
-  if (req.session.role === 'admin') next();
-  else res.status(403).send('Admin access required!!');
+  if (req.session && req.session.role === 'admin') {
+    next();
+  } else {
+    res.status(403).send('Admin access required!');
+  }
 }
+
 function isVendor(req, res, next) {
-  if (req.session.role === 'vendor') next();
-  else res.status(403).send('Vendor access required');
+  if (req.session && req.session.role === 'vendor') {
+    next();
+  } else {
+    res.status(403).send('Vendor access required!');
+  }
 }
-module.exports = { isAuthenticated, isAdmin, isVendor };
 
-
+module.exports = {
+  isAuthenticated,
+  isAdmin,
+  isVendor
+};
